21a518341f3e091881bd96ba9d4ce60d2a15e397
smilyidiot <vinayakrachha@gmail.com>
2022-09-14 09:39:17 +0000

    adding get books api

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://local:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/books", async (request, response) => {
+  const getBooksQuery = `
+    SELECT * 
+    FROM book
+    ORDER BY book_id;
+    `;
+  let booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
